5fa315d44a0a27cc1b7a943afc6678c878503566
gazisaiful07 <imsaiful2047@gmail.com>
2026-02-24 11:27:00 +0600

    fixing HTML

--- script.js
+++ script.js
@@ -5,26 +5,29 @@ let total = document.getElementById('total');
 let selectInterview = document.getElementById('interview');
 let rejectedList = document.getElementById('rejected');
 
-const filterBtn = document.getElementById('filter-btn');
-const interviewBtn = document.getElementById('interview-btn');
-const rejectionBtn = document.getElementById('reject-btn');
+const filterBtn = document.getElementById('filter-btn')
+const interviewBtn = document.getElementById('interview-btn')
+const rejectionBtn = document.getElementById('reject-btn')
 
-const cardSection = document.getElementById('cardsItems');
-const afterFilter = document.getElementById('after-filter');
+const cardSection = document.getElementById('cardsItems')
+const mainContainer = document.querySelector('main')
+
+const afterFilter = document.getElementById('after-filter')
+
 
 function jobCount() {
-    const totalCards = document.querySelectorAll('#cardsItems .border').length;
-    total.innerText = totalCards;
-    selectInterview.innerText = totalInterview.length;
-    rejectedList.innerText = totalRejection.length;
+    document.querySelectorAll('#cardsItems .border').length
+    selectInterview.innerText = totalInterview.length
+    rejectedList.innerText = totalRejection.length
 }
 
 jobCount();
 
 function toggleStyle(id) {
 
-    // reset
-    filterBtn.classList.remove('bg-blue-600', 'text-white');
+
+
+   filterBtn.classList.remove('bg-blue-600', 'text-white');
     interviewBtn.classList.remove('bg-blue-600', 'text-white');
     rejectionBtn.classList.remove('bg-blue-600', 'text-white');
 
@@ -36,100 +39,165 @@ function toggleStyle(id) {
     selected.classList.remove('bg-white', 'text-black');
     selected.classList.add('bg-blue-600', 'text-white');
 
-    afterFilter.innerHTML = '';
 
-    if (id === 'filter-btn') {
+   
+    // console.log(selected)
+
+    selected.classList.remove('bg-white', 'text-black')
+    selected.classList.add('bg-blue-600', 'text-white')
+
+     if (id === 'filter-btn') {
         cardSection.classList.remove('hidden');
         afterFilter.classList.add('hidden');
     }
 
-    if (id === 'interview-btn') {
-        cardSection.classList.add('hidden');
-        afterFilter.classList.remove('hidden');
-        renderCards(totalInterview);
-    }
+    // if (id === 'interview-btn') {
+    //     cardSection.classList.add('hidden');
+    //     afterFilter.classList.remove('hidden');
+        
+    // }
 
-    if (id === 'reject-btn') {
-        cardSection.classList.add('hidden');
-        afterFilter.classList.remove('hidden');
-        renderCards(totalRejection);
-    }
+    // if (id === 'reject-btn') {
+    //     cardSection.classList.add('hidden');
+    //     afterFilter.classList.remove('hidden');
+       
+    // }
+
 }
 
-document.querySelector('main').addEventListener('click', function (event) {
+mainContainer.addEventListener('click', function (event) {
+    // console.log(event.target.parentNode)
+    console.log(event.target.classList.contains('interview-btn'))
+    if (event.target.classList.contains('interview-btn')) {
+        const parentNode = event.target.parentNode.parentNode;
+        const jobName = parentNode.querySelector('.jobName').innerText
+        const jobPosition = parentNode.querySelector('.jobPosition').innerText
+        const jobSalary = parentNode.querySelector('.jobSalary').innerText
+        const jobStatus = parentNode.querySelector('.jobStatus').innerText
+        const jobDetails = parentNode.querySelector('.jobDetails').innerText
 
-    const card = event.target.closest('.border');
-    if (!card) return;
+        const cardDetails = {
+            jobName,
+            jobPosition,
+            jobSalary,
+            jobStatus,
+            jobDetails
+        }
+        // console.log(cardDetails)
 
-    const jobName = card.querySelector('.jobName').innerText;
-    const jobPosition = card.querySelector('.jobPosition').innerText;
-    const jobSalary = card.querySelector('.jobSalary').innerText;
-    const jobDetails = card.querySelector('.jobDetails').innerText;
-    const statusBtn = card.querySelector('.jobStatus');
 
-    const jobData = {
-        jobName,
-        jobPosition,
-        jobSalary,
-        jobDetails
-    };
+        const jobExist = totalInterview.find(i => i.jobName == cardDetails.jobName)
+        parentNode.querySelector('.jobStatus').innerText = 'Interview'
 
-    // Interview
-    if (event.target.innerText === 'Interview') {
-
-        const exist = totalInterview.find(i => i.jobName === jobName);
-        if (!exist) {
-            totalInterview.push(jobData);
+        if (!jobExist) {
+            totalInterview.push(cardDetails)
         }
 
-        statusBtn.innerText = 'Interview';
-        statusBtn.classList.remove('bg-blue-100');
-        statusBtn.classList.add('bg-green-200');
-
-        jobCount();
+        renderInterview()
     }
 
-    // Rejected
-    if (event.target.innerText === 'Rejected') {
+})
 
-        const exist = totalRejection.find(i => i.jobName === jobName);
-        if (!exist) {
-            totalRejection.push(jobData);
-        }
+function renderInterview() {
+    afterFilter.innerHTML = ''
 
-        statusBtn.innerText = 'Rejected';
-        statusBtn.classList.remove('bg-blue-100');
-        statusBtn.classList.add('bg-red-200');
+    for (let interview of totalInterview) {
 
-        jobCount();
-    }
-});
+        console.log(interview)
 
-function renderCards(dataArray) {
-
-    afterFilter.innerHTML = '';
-
-    dataArray.forEach(job => {
-
-        const div = document.createElement('div');
-        div.className = 'w-[80%] mx-auto';
-
+        let div = document.createElement('div');
+        div.className = 'w-[80%] mx-auto'
         div.innerHTML = `
-        <div class="border rounded-md border-gray-100 my-4 p-6 flex justify-between bg-white">
-            <div>
-                <div class="mb-2">
-                    <p class="text-2xl font-bold">${job.jobName}</p>
-                    <p class="text-gray-500">${job.jobPosition}</p>
+        <div>
+                <div class="border rounded-md border-gray-100 my-4 p-6 flex justify-between bg-white">
+                    <div>
+                        <div class="mb-2">
+                            <p class=" jobName text-2xl font-bold">Mobile First Corp</p>
+                            <p class=" jobPosition text-gray-500">React Native Developer</p>
+                        </div>
+                        <p class=" jobSalary mb-2 text-gray-500">Remote • Full-time • $130,000 - $175,000 </p>
+                        <div class=" mb-5">
+                            <button class=" jobStatus bg-blue-100 py-1 px-2 rounded-b-sm text-gray-900 mb-2">NOT
+                                APPLIED</button>
+                            <p class="jobDetails text-gray-700">Build cross-platform mobile applications using React
+                                Native. Work on
+                                products used by millions of
+                                users worldwide.</p>
+                        </div>
+                        <div>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase border-green-600 text-green-600 cursor-pointer interview-btn">Interview</button>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase mx-1 border-red-600 text-red-600 cursor-pointer">Rejected</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="border rounded-full p-2 border-gray-300 cursor-pointer"><i
+                                class="fa-solid fa-trash-can"></i></button>
+                    </div>
                 </div>
-                <p class="mb-2 text-gray-500">${job.jobSalary}</p>
-                <div class="mb-5">
-                    <button class="py-1 px-2 rounded-b-sm bg-gray-200 mb-2">${job.jobStatus || ''}</button>
-                    <p class="text-gray-700">${job.jobDetails}</p>
+                <div class="border rounded-md border-gray-100 my-4 p-6 flex justify-between bg-white">
+                    <div>
+                        <div class="mb-2">
+                            <p class=" jobName text-2xl font-bold">WebFlow Agency</p>
+                            <p class=" jobPosition text-gray-500">RWeb Designer & Developer</p>
+                        </div>
+                        <p class=" jobSalarymb-2 text-gray-500">Los Angeles, CA • Part-time •
+                            $80,000 - $120,000 </p>
+                        <div class=" mb-5">
+                            <button class="jobStatus bg-blue-100 py-1 px-2 rounded-b-sm text-gray-900 mb-2">NOT
+                                APPLIED</button>
+                            <p class="jobDetails text-gray-700">Create stunning web experiences for high-profile
+                                clients. Must have
+                                portfolio and experience with modern web design trends..</p>
+                        </div>
+                        <div>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase border-green-600 text-green-600 cursor-pointer">Interview</button>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase mx-1 border-red-600 text-red-600 cursor-pointer">Rejected</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="border rounded-full p-2 border-gray-300 cursor-pointer"><i
+                                class="fa-solid fa-trash-can"></i></button>
+                    </div>
                 </div>
-            </div>
-        </div>
-        `;
+                <div class="border rounded-md border-gray-100 my-4 p-6 flex justify-between bg-white">
+                    <div>
+                        <div class="mb-2">
+                            <p class=" jobName text-2xl font-bold">DataViz Solutions</p>
+                            <p class="text-gray-500 jobPosition">Data Visualization Specialist</p>
+                        </div>
+                        <p class="mb-2 text-gray-500 jobSalary">Boston, MA
+                            •
+                            Full-time
+                            •
+                            $125,000 - $165,000 </p>
+                        <div class=" mb-5">
+                            <button class="bg-blue-100 py-1 px-2 rounded-b-sm text-gray-900 mb-2 jobStatus">NOT
+                                APPLIED</button>
+                            <p class="text-gray-700 jobDetails">Transform complex data into compelling visualizations.
+                                Required skills:
+                                D3.js, React, and strong analytical thinking.
+                            </p>
+                        </div>
+                        <div>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase border-green-600 text-green-600 cursor-pointer">Interview</button>
+                            <button
+                                class="border rounded-sm py-2 px-3 font-semibold uppercase mx-1 border-red-600 text-red-600 cursor-pointer">Rejected</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="border rounded-full p-2 border-gray-300 cursor-pointer"><i
+                                class="fa-solid fa-trash-can"></i></button>
+                    </div>
+                </div>
+               
 
-        afterFilter.appendChild(div);
-    });
+            </div>
+        `
+        afterFilter.appendChild(div)
+    }
 }
